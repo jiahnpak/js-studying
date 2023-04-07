17d551e275be5b3c9d9f7d12fc96d01303f2fe2a
jiahnpak <127730558+jiahnpak@users.noreply.github.com>
2023-04-07 22:41:31 +0900

    getting weather

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,12 @@
-const weather = [];
+const API_KEY = "f16f4f6e07e28690c025c4980a9b6c23";
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  console.log(lat, long);
+}
+
+function onGeoError() {
+  alert("can't find you. NO weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
